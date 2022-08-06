f43e0b8530cc6a33f65be28c3dd4a362f89cf07f
Lucas Vieira de Albuquerque <guinho.lucas7@gmail.com.br>
2022-08-05 21:51:10 -0300

    Edit Scenes/Scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ebac.Core.Singleton;
+using DG.Tweening;
+
+public class GameManager : Singleton<GameManager>
+{
+    [Header("Player")]
+    public GameObject player;
+
+    [Header("Enemies")]
+    public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform startPoint;
+    private GameObject _currentPlayer;
+
+    [Header("Animation")]
+    public float duration;
+    public float delay;
+    public Ease ease = Ease.OutBack;
+
+    private void Start()
+    {
+        Init();
+    }
+    private void Init()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(player);
+        _currentPlayer.transform.position = startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(ease).SetDelay(delay).From();
+    }
+}
