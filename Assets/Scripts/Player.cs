f6a6911be33ce894683105600cd86d198345a021
Lucas Vieira de Albuquerque <guinho.lucas7@gmail.com.br>
2022-08-07 11:02:42 -0300

    Jump Prototype

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,17 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigdbody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float forceJump;
 
     void Update()
+    {
+        Jump();
+        Moviments();
+    }
+
+    public void Moviments()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -18,6 +25,23 @@ public class Player : MonoBehaviour
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             myRigdbody.velocity = new Vector2(-speed, myRigdbody.velocity.y);
+        }
+
+        if(myRigdbody.velocity.x > 0)
+        {
+            myRigdbody.velocity += friction;
+        }
+        else if((myRigdbody.velocity.x < 0))
+        {
+            myRigdbody.velocity -= friction;
+        }
+    }
+
+    public void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigdbody.velocity = Vector2.up * forceJump;
         }
     }
 }
