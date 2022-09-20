3a707b8df860e8332e6ede1fd0123a4ec879f0c4
Lucas Vieira de Albuquerque <guinho.lucas7@gmail.com.br>
2022-09-20 17:54:01 -0300

    updates in jump/error solved

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,8 +35,8 @@ public class Player : MonoBehaviour
 
     private bool isGrounded()
     {
-        Debug.DrawRay(transform.position, -Vector2.up, Color.magenta, distToGround + spaceToGround);
-        return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + spaceToGround);
+        Debug.DrawRay(transform.position + Vector3.up * .1f, -Vector2.up, Color.magenta, distToGround + spaceToGround);
+        return Physics2D.Raycast(transform.position + Vector3.up * .1f, -Vector2.up, distToGround + spaceToGround);
     }
 
     public void Moviments()
